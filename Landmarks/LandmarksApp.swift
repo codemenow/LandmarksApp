0fbb0cc7b6c627b4242607ed60393705335c2071
Sreekanth boddireddy <happysreekanth@gmail.com>
2021-07-04 21:40:59 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sreekanth Boddireddy on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
